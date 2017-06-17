232fe45b0104f0b550ff7b81f8f87b4dbe2cc54c
Ecila AKECHI <ecilavip@outlook.jp>
2017-06-18 04:08:52 +0900

    [update] staticにした

--- src/main/java/life/grass/grasscreatures/treasure/TreasureHolder.java
+++ src/main/java/life/grass/grasscreatures/treasure/TreasureHolder.java
@@ -6,22 +6,22 @@ import java.util.HashMap;
  * Created by ecila on 2017/06/18.
  */
 public class TreasureHolder {
-    private HashMap<String, DropTable> dropTables;
-    private HashMap<String, LootTable> lootTables;
+    private static HashMap<String, DropTable> dropTables;
+    private static HashMap<String, LootTable> lootTables;
 
-    public DropTable getDropTable(String key) {
+    public static DropTable getDropTable(String key) {
         return dropTables.containsKey(key) ? dropTables.get(key) : null;
     }
 
-    public LootTable getLootList(String key) {
+    public static LootTable getLootList(String key) {
         return lootTables.containsKey(key) ? lootTables.get(key) : null;
     }
 
-    public void putDropTable(DropTable table) {
+    public static void putDropTable(DropTable table) {
         dropTables.put(table.getTableName(), table);
     }
 
-    public void putLootTable(LootTable table) {
+    public static void putLootTable(LootTable table) {
         lootTables.put(table.getLootTableName(), table);
     }
 }
