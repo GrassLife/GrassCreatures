9624d0db45ca0752aec588b7f795ad664c30825e
明地えしら <ecilavip@outlook.jp>
2017-06-18 03:51:12 +0900

    initial commit

---
+++ src/main/java/life/grass/grasscreatures/GrassCreatures.java
@@ -0,0 +1,17 @@
+package life.grass.grasscreatures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GrassCreatures extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+//        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
